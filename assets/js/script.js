b1fed8627aaad788f273fe978e346643bde69382
Adam Santillana <adamsantillana@AdamMacMax.local>
2022-12-03 19:30:12 -0600

    Added comments to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-// API Key
+// Initializing variables
+// Also retrieving the cityStorage from localStorage and putting it into an array.
 var APIKey = "140868a0bfdca5e4838b5700f0881180";
 var city;
 var queryURL;
@@ -6,30 +7,21 @@ var geoQuery;
 var lat;
 var long;
 var prevCities = document.getElementById('previous_cities');
-
 var cityStorage = window.localStorage.getItem("City")
 ? JSON.parse(window.localStorage.getItem("City"))
 : [];
 
 //Getting and displaying the text for the upcoming five days of the week
 var d = new Date();
-var day1 =d.getDate();
-var month1 = d.getMonth();
-var year1 = d.getFullYear();
-
 var day0 = d.getDate();
 var month0 = d.getMonth();
 var year0 = d.getFullYear();
-
-
 if(month0 < 12){
     month0 = month0 + 1;
 }
 else{
     month0 = 12;
 };
-
-console.log(d);
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
 //Function to get the correct integer for the index of the days array
@@ -41,51 +33,54 @@ function CheckDay(day){
         return day + d.getDay();
     }
 }
-
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i+1)];
     }
     
-
+// Updates the Para2 heading with current Weather and date.
 document.querySelector('#Para2').innerHTML = "Current Weather" + ' ('+ month0 +'/'+ day0 +'/' +year0 +')';
 
-// Function to print cities from history at bottom of page.   
+// Function to print previous cities as anchor tags.
 console.log(cityStorage);
 let uniqueCities = [...new Set(cityStorage)];
 console.log(uniqueCities);
-
     for (var i = 0; i < uniqueCities.length; i++) {
         if (uniqueCities[i] === '') {
             console.log('null');
         }
         else {
-
-        
     var entry = document.createElement('li');
                                      // Create anchor element.
                                      var a = document.createElement('a'); 
           
                                      // Create the text node for anchor element.
                                      var link = document.createTextNode(uniqueCities[i]);
-                                       
+                                     
                                      // Append the text node to anchor element.
                                      a.appendChild(link); 
-                                       
+                                       var oneCity = a.textContent;
                                      // Set the title.
                                      a.title = uniqueCities[i]; 
                                        
                                      // Set the href property.
-                                     a.href = "javascript:getCurrentWeather(uniqueCities[i]);"; 
+                                    a.href = "javascript:getAnchor();"; 
+
+                                      // Set the onclick
+
+                                      a.onclick = function () { 
+                                        getCurrentWeather('Dallas');
+                                        }
                                        
                                      // Append the anchor element to the body.
                                      entry.appendChild(a); 
+                                     console.log(oneCity);
 
         prevCities.appendChild(entry);
     }
 }
 
 
-// Weather API placeholder
+// Event listenter for user pressing Enter.
 var searchValue = document.querySelector('#cityInput');
 searchValue.addEventListener('keypress', setFunc);
     function setFunc(e) {
@@ -95,35 +90,44 @@ searchValue.addEventListener('keypress', setFunc);
         }
     }
 
- // search button function
+ // Event listenter for user clicking search button.
 
      function getInfo() {
          getCurrentWeather(searchValue.value);
      }
 
-// getcurrentWeatherfunction
 
+// anchor tag function
+// MUST FIX: currently doesn't work.
+function getAnchor() {
+    // traverse the DOM and get the url from the title of anchor tag.
+    // Find out how to get the text content of the anchor tag that was clicked and pass that into URLCity
+    var URLCity = 'Dallas';
+    getCurrentWeather('Dallas');
+}
+
+// getcurrentWeatherfunction
 function getCurrentWeather(city) {
-    city = searchValue.value;
     var day0 = d.getDate();
     var month0 = d.getMonth();
     var year0 = d.getFullYear();
-    
-    
+
     if(month0 < 12){
         month0 = month0 + 1;
     }
     else{
         month0 = 12;
     };
-
-    document.querySelector('#city').innerHTML = city+ ' ('+ month0 +'/'+ day0 +'/' +year0 +')';
+// Puts city into current weather container.
+    document.querySelector('#city').innerHTML = city;
 
     if (!cityStorage.includes(city)) {
         cityStorage.push(city);
         localStorage.setItem("City", city);
         console.log(localStorage.getItem("City"));
          localStorage.setItem("City", JSON.stringify(cityStorage));
+
+         // Adds the current city to list of previous cities.
             var entry = document.createElement('li');
                                          // Create anchor element.
                                          var a = document.createElement('a'); 
@@ -137,23 +141,28 @@ function getCurrentWeather(city) {
                                          // Set the title.
                                          a.title = city; 
                                            
-                                         // Set the href property.
-                                         a.href = "javascript:getCurrentWeather(Dallas);"; 
-                                           
+                                           // Set the href property.
+                                    a.href = "javascript:getAnchor();"; 
+
+                                    // Set the onclick
+
+                                    a.onclick = function () { 
+                                      getCurrentWeather('Dallas');
+                                      }
+
                                          // Append the anchor element to the body.
                                          entry.appendChild(a); 
     
             prevCities.appendChild(entry);
     
     }
-
+// create fetch url to retrieve the latitude and longitude  of city.
     geoQuery = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city+ '&limit=5&appid=' + APIKey;
+   // calls getLatLong function and passes city input by user.
     getLatLong(city)
+    // calls getForecast and passes city input by user.
     getForecast(city)
 }
-
-
-
 
     function getLatLong(city) {
         fetch(geoQuery)
@@ -161,11 +170,11 @@ function getCurrentWeather(city) {
             return response.json();
         })
         .then(function (latlong) {
-            
+            // Sets latitude and longitude of city.
             lat = latlong[0].lat;
             
             long = latlong[0].lon;
-            
+            // checks weather based on latitude and longitude of city.
             getData(lat, long);
         })
         .catch(function (err) {
@@ -175,9 +184,8 @@ function getCurrentWeather(city) {
     }
     function getData(lat, long) {
         var lat1 = lat;
-        console.log(lat1);
         var long2 = long;
-        console.log(long2);
+        // calls queryURL to retrieve current weaather based on latitude and longitude.
         queryURL = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat1 + '&lon=' + long2 +'&units=imperial&exclude=hourly,minutely,daily&appid=' + APIKey;
         fetch(queryURL)
         .then (function (response) {
@@ -195,6 +203,7 @@ function getCurrentWeather(city) {
         })
     }
 
+    // Gets 5 day weather using queryURL of City.
    function getForecast(city) {
     queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid="+APIKey;
     fetch(queryURL)
